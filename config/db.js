989e7af3ccc6cb1380f2b7e2398d45f7dab9dda5
caiolariel <cgfuentelima0277@gmail.com>
2025-04-03 22:52:07 -0300

    fix: correção no env

--- config/db.js
+++ config/db.js
@@ -1,21 +1,22 @@
+require('dotenv').config(); 
 const mysql = require('mysql2');
 
 // Criação da conexão com o banco de dados
 const connection = mysql.createConnection({
-    host: 'www.thyagoquintas.com.br',
-    user: 'engenharia_40',
-    password: 'lontragigante',
-    database: 'engenharia_40',
-    port: 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 // Conectar ao banco de dados
 connection.connect((err) => {
     if (err) {
         console.error('Erro ao conectar ao banco de dados:', err);
-        process.exit();
+        process.exit(1);
     }
-    console.log('Conectado ao banco de dados');
+    console.log('✅ Conectado ao banco de dados');
 });
 
 module.exports = connection;
