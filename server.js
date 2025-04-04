9486f389a1fbb45eea7a18d7a58df1f4558085c3
caiolariel <cgfuentelima0277@gmail.com>
2025-04-03 23:12:28 -0300

    fix: correção servidor

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ const cors = require('cors');
 const pedidoRoutes = require('./routes/pedidoRoutes');
 
 const app = express();
+const cors = require('cors');
 
-// Middleware
-app.use(cors({ origin: '*' })); 
-app.use(express.json()); 
+app.use(cors({
+    origin: '*',  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
+app.use(express.json());
 
 // Rotas
 app.use('/api', pedidoRoutes);
 
-const PORT = process.env.PORT || 8080;  // Railway define a porta dinamicamente
+const PORT = process.env.PORT || 3000;  // Railway define a porta dinamicamente
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
