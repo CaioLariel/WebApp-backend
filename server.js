cda6a7b00e9ee82e0c94932080c4e9df927cc3c8
caiolariel <cgfuentelima0277@gmail.com>
2025-04-03 22:57:33 -0300

    fix: correção no middleware

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
+require('dotenv').config(); // Carrega as variáveis do .env no início
+
 const express = require('express');
 const cors = require('cors');
 const pedidoRoutes = require('./routes/pedidoRoutes');
-const dotenv = require('dotenv');
-
-// Configurar variáveis de ambiente
-dotenv.config();
-
 
 const app = express();
 
 // Middleware
-app.use(cors());  
-app.use(express.json());  
+app.use(cors({ origin: '*' })); 
+app.use(express.json()); 
 
 // Rotas
 app.use('/api', pedidoRoutes);
 
-// Iniciar o servidor
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
